e8f5dc6bdffef20a5a29927e05eec9ab23ce101c
Grant Wilsey <99307736+darkyolks@users.noreply.github.com>
2025-03-01 21:28:46 -0800

    Remove _site from .gitignore

---
+++ _site/js/common.js
@@ -0,0 +1,177 @@
+$(document).ready(function() {
+  'use strict';
+
+  var menuOpenIcon = $(".nav__icon-menu"),
+    menuCloseIcon = $(".nav__icon-close"),
+    menuList = $(".menu-overlay"),
+    searchOpenIcon = $(".search-button"),
+    searchCloseIcon = $(".search__close"),
+    searchInput = $(".search__text"),
+    searchBox = $(".search");
+
+
+  /* =======================
+  // Menu and Search
+  ======================= */
+  menuOpenIcon.click(function () {
+    menuOpen();
+  })
+
+  menuCloseIcon.click(function () {
+    menuClose();
+  })
+
+  searchOpenIcon.click(function () {
+    searchOpen();
+  });
+
+  searchCloseIcon.click(function () {
+    searchClose();
+  });
+
+  function menuOpen() {
+    menuList.addClass("is-open");
+  }
+
+  function menuClose() {
+    menuList.removeClass("is-open");
+  }
+
+  function searchOpen() {
+    searchBox.addClass("is-visible");
+    setTimeout(function () {
+      searchInput.focus();
+    }, 300);
+  }
+
+  function searchClose() {
+    searchBox.removeClass("is-visible");
+  }
+
+  $('.search, .search__box').on('click keyup', function (event) {
+    if (event.target == this || event.keyCode == 27) {
+      $('.search').removeClass('is-visible');
+    }
+  });
+
+
+  /* =======================
+  // Animation Load Page
+  ======================= */
+  setTimeout(function(){
+    $('body').addClass('is-in');
+  },150)
+
+
+  // =====================
+  // Initialize Simple Jekyll Search
+  SimpleJekyllSearch({
+    searchInput: document.getElementById("js-search-input"),
+    resultsContainer: document.getElementById("js-results-container"),
+    json: "/search.json",
+    searchResultTemplate: `
+      <div class="article col col-4 col-d-6 col-t-12 grid__post animate">
+        <div class="article__inner">
+          <a class="article__image" href="{url}">
+            <img src="{image}" alt="{title}">
+          </a>
+          <div class="article__content">
+            <div class="article__meta">
+              <span class="article__minutes">{words}</span>
+            </div>
+            <h2 class="article__title"><a href="{url}">{title}</a></h2>
+            <div class="article__bottom">
+              <div class="article__bottom-meta">
+                <div class="article-tags">{tags}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `,
+    templateMiddleware: function(prop, value) {
+      if (prop === 'tags' && Array.isArray(value)) {
+        return value.map(tag => 
+          `<a href="/tags/?tag=${tag}" class="article__tag">${tag}</a>`
+        ).join(' ');
+      }
+      return value;
+    },
+    noResultsText: '<div class="no-results"><h3>No results found</h3></div>',
+    fuzzy: false
+  });
+
+
+  /* =======================
+  // LazyLoad Images
+  ======================= */
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: '.lazy'
+  })
+
+
+  // =====================
+  // Ajax Load More
+  // =====================
+  var $load_posts_button = $('.load-more-posts');
+
+  $load_posts_button.click(function(e) {
+    e.preventDefault();
+    var loadMore = $('.load-more-section');
+    var request_next_link = pagination_next_url.split('/page')[0] + '/page/' + pagination_next_page_number + '/';
+
+    $.ajax({
+      url: request_next_link,
+      beforeSend: function() {
+        $load_posts_button.text('Loading...');
+      }
+    }).done(function(data) {
+      var posts = $('.grid__post', data);
+      $('.grid').append(posts);
+
+      var lazyLoadInstance = new LazyLoad({
+        elements_selector: '.lazy'
+      })
+
+      $load_posts_button.text('Load more');
+      pagination_next_page_number++;
+
+      if (pagination_next_page_number > pagination_available_pages_number) {
+        loadMore.addClass('hide');
+      }
+    });
+  });
+
+
+  /* =======================
+  // Responsive Videos
+  ======================= */
+  $(".post__content, .page__content").fitVids({
+    customSelector: ['iframe[src*="ted.com"]', 'iframe[src*="player.twitch.tv"]', 'iframe[src*="facebook.com"]']
+  });
+
+
+  /* =======================
+  // Zoom Image
+  ======================= */
+  $(".page img, .post img").attr("data-action", "zoom");
+  $(".page a img, .post a img").removeAttr("data-action", "zoom");
+
+
+  /* =======================
+  // Scroll Top Button
+  ======================= */
+  $(".top").click(function() {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > $(window).height()) {
+      $(".top").addClass("is-active");
+    } else {
+      $(".top").removeClass("is-active");
+    }
+  });
+
+});
